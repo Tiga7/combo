a4730d3c93ee0f87fbc6dbe710e982f4c489a288
gangan <735180443@qq.com>
2022-12-17 22:42:41 +0800

    finish basic job

---
+++ game/static/js/src/menu/z_base.js
@@ -0,0 +1,12 @@
+class AcGameMenu{
+    constructor(root){
+        this.root = root
+        this.$menu = $(`
+            <div class="ac-game-menu">
+            
+            </div>
+        `);
+    }
+
+}
+
--- game/views.py
+++
@@ -1,8 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-    return HttpResponse("hello,welcome!")
-
-def play(request):
-    return HttpResponse("go and play")
-
